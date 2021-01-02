fe48c33abfae2f1b5a3ade48ce36ffa6ddf84a6d
guoguo <noobwu@126.com>
2021-01-02 20:44:13 +0800

    一个简单的神经网络
    一个简单的神经网络

---
+++ Chapter02/Example1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Console = Colorful.Console;
+
+namespace NeuralNetwork
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            NNManager mgr = new NNManager();
+            mgr.SetupNetwork()
+                .GetTrainingDataFromUser()
+                .TrainNetworkToMinimum()
+                .TestNetwork();
+
+            Console.WriteLine("Press any key to train network for maximum");
+            Console.ReadKey();
+
+            mgr.SetupNetwork()
+                .GetTrainingDataFromUser()
+                .TrainNetworkToMaximum()
+                .TestNetwork();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+
+        }
+    }
+}
